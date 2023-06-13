9db9c8f9540f63a6a627046a7e50a53768e2e804
Yunus Emre Daysal <yunusemredaysal@gmail.com>
2023-06-13 22:26:30 +0300

    Organize variables.tf

--- infrastructure/terraform/aws/ec2/variables.tf
+++ infrastructure/terraform/aws/ec2/variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
-  type        = string
-  default     = "eu-central-1"
+  type    = string
+  default = "eu-central-1"
 }
 
 variable "region_a" {
-  type        = string
-  default     = "eu-central-1a"
+  type    = string
+  default = "eu-central-1a"
 }
 
 variable "public_key" {
