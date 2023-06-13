8d4be959204df9817177e0d9417dc97950c68c63
Yunus Emre Daysal <yunusemredaysal@gmail.com>
2023-06-13 21:51:29 +0300

    Update cybersift vpc's route table to connect to the Internet

--- infrastructure/terraform/aws/ec2/main.tf
+++ infrastructure/terraform/aws/ec2/main.tf
@@ -23,3 +23,16 @@ resource "aws_internet_gateway" "cybersift" {
     Name = "cybersift-vpc-internet-gw"
   }
 }
+
+resource "aws_default_route_table" "cybersift" {
+  default_route_table_id = aws_vpc.cybersift.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.cybersift.id
+  }
+
+  tags = {
+    Name = "cybersift-vpc-route-table"
+  }
+}
