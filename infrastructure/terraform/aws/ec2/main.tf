26f734680d226f80113586d30e79f23c75bcf3f2
Yunus Emre Daysal <yunusemredaysal@gmail.com>
2023-06-13 22:03:01 +0300

    Create cybersift ec2 instance

--- infrastructure/terraform/aws/ec2/main.tf
+++ infrastructure/terraform/aws/ec2/main.tf
@@ -84,3 +84,17 @@ resource "aws_network_interface" "cybersift" {
     Name = "cybersift-instance-primary-nic"
   }
 }
+
+resource "aws_instance" "cybersift" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  
+  network_interface {
+    network_interface_id = aws_network_interface.cybersift.id
+    device_index = 0
+  }
+
+  tags = {
+    Name = "cybersift"
+  }
+}
