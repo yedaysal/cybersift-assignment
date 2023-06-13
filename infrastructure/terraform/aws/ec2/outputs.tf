177f0efc73b407c37766bc54afa59d70187015d3
Yunus Emre Daysal <yunusemredaysal@gmail.com>
2023-06-13 22:11:50 +0300

    Create an output to print created vpc id

--- infrastructure/terraform/aws/ec2/outputs.tf
+++ infrastructure/terraform/aws/ec2/outputs.tf
@@ -2,3 +2,9 @@ output "aws_ami_name" {
   description = "The name of the ami"
   value = data.aws_ami.ubuntu.name
 }
+
+output "vpc_id" {
+  description = "ID of the created vpc"
+  value = aws_vpc.cybersift.id
+}
+
