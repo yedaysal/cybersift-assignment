13f5fc424c0e876bb6e07fc4997d98aab6e7a4dc
Yunus Emre Daysal <yunusemredaysal@gmail.com>
2023-06-16 00:12:29 +0300

    Open Grafana ui port 3000 on fw for public access

--- infrastructure/terraform/aws/ec2/main.tf
+++ infrastructure/terraform/aws/ec2/main.tf
@@ -80,6 +80,14 @@ resource "aws_default_security_group" "cybersift" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Grafana"
+    from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port = 0
