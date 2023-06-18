60505ccb08d576bce559406116935fe36f6a64b9
Yunus Emre Daysal <yunusemredaysal@gmail.com>
2023-06-18 14:13:04 +0300

    Remove unused files and entries from docker-elastic role

--- infrastructure/ansible/roles/docker-elastic/files/docker-elk/setup/entrypoint.sh
+++ infrastructure/ansible/roles/docker-elastic/files/docker-elk/setup/entrypoint.sh
@@ -5,40 +5,15 @@ set -o pipefail
 
 source "${BASH_SOURCE[0]%/*}"/lib.sh
 
-
 # --------------------------------------------------------
 # Users declarations
 
 declare -A users_passwords
 users_passwords=(
-	[logstash_internal]="${LOGSTASH_INTERNAL_PASSWORD:-}"
 	[kibana_system]="${KIBANA_SYSTEM_PASSWORD:-}"
-	[metricbeat_internal]="${METRICBEAT_INTERNAL_PASSWORD:-}"
-	[heartbeat_internal]="${HEARTBEAT_INTERNAL_PASSWORD:-}"
-	[monitoring_internal]="${MONITORING_INTERNAL_PASSWORD:-}"
-	[beats_system]="${BEATS_SYSTEM_PASSWORD=:-}"
-)
-
-declare -A users_roles
-users_roles=(
-	[logstash_internal]='logstash_writer'
-	[metricbeat_internal]='metricbeat_writer'
-	[heartbeat_internal]='heartbeat_writer'
-	[monitoring_internal]='remote_monitoring_collector'
 )
 
 # --------------------------------------------------------
-# Roles declarations
-
-declare -A roles_files
-roles_files=(
-	[logstash_writer]='logstash_writer.json'
-	[metricbeat_writer]='metricbeat_writer.json'
-	[heartbeat_writer]='heartbeat_writer.json'
-)
-
-# --------------------------------------------------------
-
 
 log 'Waiting for availability of Elasticsearch. This can take several minutes.'
 
