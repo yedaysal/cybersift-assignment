556f2327e617f3deb2f0267e16d181d2d9ea82f7
Yunus Emre Daysal <yunusemredaysal@gmail.com>
2023-06-18 13:38:02 +0300

    Remove all related to unused filebeat comp. from docker-elastic role

--- infrastructure/ansible/roles/docker-elastic/files/docker-elk/setup/entrypoint.sh
+++ infrastructure/ansible/roles/docker-elastic/files/docker-elk/setup/entrypoint.sh
@@ -14,7 +14,6 @@ users_passwords=(
 	[logstash_internal]="${LOGSTASH_INTERNAL_PASSWORD:-}"
 	[kibana_system]="${KIBANA_SYSTEM_PASSWORD:-}"
 	[metricbeat_internal]="${METRICBEAT_INTERNAL_PASSWORD:-}"
-	[filebeat_internal]="${FILEBEAT_INTERNAL_PASSWORD:-}"
 	[heartbeat_internal]="${HEARTBEAT_INTERNAL_PASSWORD:-}"
 	[monitoring_internal]="${MONITORING_INTERNAL_PASSWORD:-}"
 	[beats_system]="${BEATS_SYSTEM_PASSWORD=:-}"
@@ -24,7 +23,6 @@ declare -A users_roles
 users_roles=(
 	[logstash_internal]='logstash_writer'
 	[metricbeat_internal]='metricbeat_writer'
-	[filebeat_internal]='filebeat_writer'
 	[heartbeat_internal]='heartbeat_writer'
 	[monitoring_internal]='remote_monitoring_collector'
 )
@@ -36,7 +34,6 @@ declare -A roles_files
 roles_files=(
 	[logstash_writer]='logstash_writer.json'
 	[metricbeat_writer]='metricbeat_writer.json'
-	[filebeat_writer]='filebeat_writer.json'
 	[heartbeat_writer]='heartbeat_writer.json'
 )
 
